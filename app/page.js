2274e8782073a4655e604805f607aa96bcb6fdcc
Vbeni <benivillafana@gmail.com>
2023-09-27 16:39:07 -0700

    added financial inputs

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
 export default function Home(){
   return (
     <div>
-      <h1>Financial Advisor Home</h1>
+      <h1>Financial Inputs</h1>
+
+      <form>
+        <label>Monthly Income</label>
+        <label>Monthly Expenses</label>
+        <label>Financial Goals ($)</label>
+
+        <button type="submit">Get Insights</button>
+      </form>
     </div>
+
   )
 }

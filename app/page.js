12a77b09102304a70cc910a3fbaf0bb8fdb1cf58
Vbeni <benivillafana@gmail.com>
2023-09-27 16:53:23 -0700

    added styling to financial inputs form

--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
-export default function Home(){
+export default function Home() {
   return (
-    <div>
-      <h1>Financial Inputs</h1>
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <div className="bg-black p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl mb-4 text-gray-400">Financial Inputs</h1>
+        
+        <form>
+          <div className="mb-4">
+            <label className="block mb-2 text-gray-400">Monthly Income</label>
+            <input type="number" className="p-2 w-full border rounded text-gray-400" />
+          </div>
+          
+          <div className="mb-4">
+            <label className="block mb-2 text-gray-400">Monthly Expenses</label>
+            <input type="number" className="p-2 w-full border rounded text-gray-400" />
+          </div>
+          
+          <div className="mb-4">
+            <label className="block mb-2 text-gray-400">Financial Goals ($)</label>
+            <input type="number" className="p-2 w-full border rounded text-gray-400" />
+          </div>
 
-      <form>
-        <label>Monthly Income</label>
-        <label>Monthly Expenses</label>
-        <label>Financial Goals ($)</label>
-
-        <button type="submit">Get Insights</button>
-      </form>
+          <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+            Get Insights
+          </button>
+        </form>
+      </div>
     </div>
-
-  )
+  );
 }
